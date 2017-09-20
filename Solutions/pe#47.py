a67454bcc0739d614c476cb2afa9cfe9da1b6272
Abhishek Choudhary <2012ecs08@smvdu.ac.in>
2017-09-20 16:42:56 +0530

    optimize a little
    change the condition operator from >= to '===' which saves about 0.8 seconds in the execution time

--- Solutions/pe#47.py
+++ Solutions/pe#47.py
@@ -2,7 +2,7 @@
 Technique is brute-force.
 1. A function to return the number of prime factors of a number.
 2. Start iteration from (2*3*5*7) and continue.
-3. Execution time - 2.654 
+3. Execution time - 1.802 
 """
 
 
@@ -27,7 +27,7 @@ consec = 1
 
 while consec < 4:
   result += 1
-  if numberOfPrimeFactors(result) >= 4:
+  if numberOfPrimeFactors(result) == 4:
     consec += 1
   else:
     consec = 0
